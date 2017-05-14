cf9c900274d111a32e868dfc3655416c39b30511
Greg Steckman <gsteckman@gmail.com>
2017-05-13 19:16:34 -0700

    Refactor door control out to a separate project.

--- src/main/java/io/github/gsteckman/rpi_rest/App.java
+++ src/main/java/io/github/gsteckman/rpi_rest/App.java
@@ -5,49 +5,63 @@ package io.github.gsteckman.rpi_rest;
  * 
  * Copyright 2017 Greg Steckman
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at:
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed 
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
  * implied. See the License for the specific language governing permissions and limitations under the License.
  *
  */
 
-import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioProvider;
 import com.pi4j.io.gpio.RaspiGpioProvider;
 import com.pi4j.io.gpio.RaspiPinNumberingScheme;
-import com.pi4j.wiringpi.GpioUtil;
 
 /**
- * This class serves as the application entry point and Spring Framework Boot configuration for the application. 
+ * This class serves as a Spring Framework Boot configuration for the base rpi-rest interface. It should be subclassed
+ * to implement the gpioController method and optionally a main method.
+ * 
+ * The gpioController method of the subclass should create and configure the GpioController object for the GPIO pins
+ * needed by the application and their corresponding direction (input/output), as illustrated in the example below that
+ * provisions and configures pins 4 and 17 for output:
+ * 
+ * <code>
+ * &#64;Bean
+ * &#64;Override
+ * public GpioController gpioController(final GpioProvider gp) {
+ *    GpioFactory.setDefaultProvider(gp);
+ *    GpioController gpio = GpioFactory.getInstance();
+ *    GpioPin pin4 = gpio.provisionDigitalOutputPin(RaspiBcmPin.GPIO_04, PinState.LOW);
+ *    pin4.setShutdownOptions(true, PinState.LOW);
+ *    GpioPin pin17 = gpio.provisionDigitalOutputPin(RaspiBcmPin.GPIO_17, PinState.LOW);
+ *    pin17.setShutdownOptions(true, PinState.LOW);
+ *    return gpio;
+ * }
+ * </code>
+ * 
+ * The subclass' optional main method is used to bootstrap the application, and if using the Spring Boot framework would
+ * be similar to the below code:
+ * 
+ * <code>
+ * public static void main(String[] args) {
+ *    GpioUtil.enableNonPrivilegedAccess(); //See pi4j documentation for details
+ *    SpringApplication app = new SpringApplication(AppSubclass.class); //Where "AppSubclass" is the name of the subclass
+ *    app.run(args);
+ * }
+ * </code>
+ * 
  * @author Greg Steckman
  *
  */
 @SpringBootApplication
-public class App {
-    /**
-     * Application entry point.
-     * @param args Command line arguments.
-     */
-    public static void main(String[] args) {
-        GpioUtil.enableNonPrivilegedAccess();
-        SpringApplication app = new SpringApplication(App.class);
-        app.setBannerMode(Banner.Mode.OFF);
-        app.run(args);
-    }
-
-    public App() {
-
-    }
-
+public abstract class App {
     /**
      * @return The SsdpHandler to be used by the application.
      */
@@ -65,19 +79,21 @@ public class App {
     }
 
     /**
-     * @param gp The GpioProvider to be used by the controller.
+     * This method should be overridden by a subclass that creates a GpioController with the required pins provisioned
+     * for input/output as needed by the end application.
+     * 
+     * @param gp
+     *            The GpioProvider to be used by the controller.
      * @return The GpioController to be used by the application.
      */
     @Bean
-    public GpioController gpioController(final GpioProvider gp) {
-        GpioFactory.setDefaultProvider(gp);
-        GpioController gpio = GpioFactory.getInstance();
-        return gpio;
-    }
+    public abstract GpioController gpioController(final GpioProvider gp);
 
     /**
      * Creates and returns the RestGpioController bean.
-     * @param gc GpioController to be used by the RestGpioController.
+     * 
+     * @param gc
+     *            GpioController to be used by the RestGpioController.
      * @return A new RestGpioController.
      */
     @Bean
--- src/main/java/io/github/gsteckman/rpi_rest/RestGpioController.java
+++ src/main/java/io/github/gsteckman/rpi_rest/RestGpioController.java
@@ -5,6 +5,24 @@ package io.github.gsteckman.rpi_rest;
  * 
  * Copyright 2017 Greg Steckman
  *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance 
+ * with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License 
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+import java.util.Collection;
+
+/*
+ * RestGpioController.java
+ * 
+ * Copyright 2017 Greg Steckman
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at:
  *
  * http://www.apache.org/licenses/LICENSE-2.0
@@ -33,11 +51,13 @@ import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioPin;
 import com.pi4j.io.gpio.GpioPinDigital;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
-import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiBcmPin;
 
 /**
- * This class implements a REST interface to the Raspberry Pi GPIO.
+ * This class implements a REST interface to the Raspberry Pi GPIO. Methods that read/write the GPIO are synchronized on
+ * the GpioController object so as to avoid concurrent access from multiple threads. Other users of the GpioController
+ * instance should take care to prevent concurrent access with this class' instance by also synchronizing on the object
+ * or with other suitable mechanisms.
  * 
  * @author Greg Steckman
  *
@@ -46,9 +66,6 @@ import com.pi4j.io.gpio.RaspiBcmPin;
 public class RestGpioController {
     private static final Log LOG = LogFactory.getLog(RestGpioController.class);
     private GpioController gpio;
-    private boolean doorState = false;
-    private GpioPinDigitalOutput pin4;
-    private GpioPinDigitalOutput pin17;
 
     /**
      * Creates a new instance using the specified GpioController.
@@ -58,72 +75,11 @@ public class RestGpioController {
      */
     public RestGpioController(final GpioController gpioCtrl) {
         gpio = gpioCtrl;
-        pin4 = gpio.provisionDigitalOutputPin(RaspiBcmPin.GPIO_04, PinState.LOW);
-        pin4.setShutdownOptions(true, PinState.LOW);
-        pin17 = gpio.provisionDigitalOutputPin(RaspiBcmPin.GPIO_17, PinState.LOW);
-        pin17.setShutdownOptions(true, PinState.LOW);
     }
 
     /**
-     * POST /door REST endpoint to open/close the door.
-     * 
-     * Body
-     * 
-     * {action: "open" | "close"}
-     * 
-     * Response:
-     * 
-     * { name: "door", state: "open" | "closed" }
-     * 
-     * @param model
-     *            JSON data model provided in the body of the POST
-     */
-    @PostMapping(path = "/door")
-    public Map<String, Object> door(@RequestBody Map<String, Object> model) {
-        LOG.debug("POST door");
-        String action = (String) model.get("action");
-        if ("open".equals(action)) {
-            synchronized (gpio) {
-                pin4.setState(false);
-                pin17.setState(false);
-                pin4.pulse(100, true);
-                doorState = true;
-            }
-        } else {
-            synchronized (gpio) {
-                pin4.setState(false);
-                pin17.setState(false);
-                pin17.pulse(100, true);
-                doorState = false;
-            }
-        }
-        return getDoorState();
-    }
-
-    /**
-     * 
-     * Response:
-     * 
-     * {name: "door", state: "open" | "closed"}
-     * 
-     * @return Map of name/value pairs that Spring converts to a HTTP JSON Response
-     */
-    @GetMapping(path = "/door")
-    public Map<String, Object> getDoorState() {
-        LOG.debug("GET door");
-
-        Map<String, Object> returnMap = new HashMap<String, Object>();
-        returnMap.put("name", "door");
-        if (doorState) {
-            returnMap.put("state", "open");
-        } else {
-            returnMap.put("state", "closed");
-        }
-        return returnMap;
-    }
-
-    /**
-     * GPIOS POST handler. POST parameter named "state" should be provided in the POST body to set the value either "HIGH" or "LOW"
+     * GPIOS POST handler. POST parameter named "state" should be provided in the POST body to set the value either
+     * "HIGH" or "LOW"
      * 
      * @param pinName
      *            Name of Pin to be set/cleared
@@ -134,19 +90,21 @@ public class RestGpioController {
     @PostMapping(path = "/gpios/{address}")
     public Map<String, Object> setGpio(@PathVariable int address, @RequestBody Map<String, String> model) {
         LOG.debug("setGpio");
-        GpioPin pin = gpio.getProvisionedPin(RaspiBcmPin.getPinByAddress(address));
-        if (pin != null) {
-            if (pin instanceof GpioPinDigitalOutput) {
-                if (model.get("state") != null && model.get("state").toUpperCase().equals("HIGH")) {
-                    ((GpioPinDigitalOutput) pin).setState(true);
-                } else {
-                    ((GpioPinDigitalOutput) pin).setState(false);
+        synchronized (gpio) {
+            GpioPin pin = gpio.getProvisionedPin(RaspiBcmPin.getPinByAddress(address));
+            if (pin != null) {
+                if (pin instanceof GpioPinDigitalOutput) {
+                    if (model.get("state") != null && model.get("state").toUpperCase().equals("HIGH")) {
+                        ((GpioPinDigitalOutput) pin).setState(true);
+                    } else {
+                        ((GpioPinDigitalOutput) pin).setState(false);
+                    }
                 }
+                if (pin instanceof GpioPinDigital)
+                    return mapFromPinState((GpioPinDigital) pin);
+                else
+                    return mapFromPinState(pin);
             }
-            if (pin instanceof GpioPinDigital)
-                return mapFromPinState((GpioPinDigital) pin);
-            else
-                return mapFromPinState(pin);
         }
         return null;
     }
@@ -161,12 +119,14 @@ public class RestGpioController {
     @GetMapping(path = "/gpios/{address}")
     public Map<String, Object> getGpio(@PathVariable int address) {
         LOG.debug("getGpio");
-        GpioPin pin = gpio.getProvisionedPin(RaspiBcmPin.getPinByAddress(address));
-        if (pin != null) {
-            if (pin instanceof GpioPinDigital)
-                return mapFromPinState((GpioPinDigital) pin);
-            else
-                return mapFromPinState(pin);
+        synchronized (gpio) {
+            GpioPin pin = gpio.getProvisionedPin(RaspiBcmPin.getPinByAddress(address));
+            if (pin != null) {
+                if (pin instanceof GpioPinDigital)
+                    return mapFromPinState((GpioPinDigital) pin);
+                else
+                    return mapFromPinState(pin);
+            }
         }
         return null;
     }
@@ -174,14 +134,19 @@ public class RestGpioController {
     /**
      * Provides the status of all GPIO pins.
      * 
-     * @return A list of Maps of pin state attributes, one per pin, which Spring Framework converts to a JSON formatted HTTP response.
+     * @return A list of Maps of pin state attributes, one per pin, which Spring Framework converts to a JSON formatted
+     *         HTTP response.
      */
     @GetMapping(path = "/gpios")
     public List<Map<String, Object>> getGpios() {
         LOG.debug("getGpios");
         List<Map<String, Object>> l = new LinkedList<Map<String, Object>>();
-        l.add(mapFromPinState(pin4));
-        l.add(mapFromPinState(pin17));
+        synchronized (gpio) {
+            Collection<GpioPin> pins = gpio.getProvisionedPins();
+            for (GpioPin pin : pins) {
+                l.add(mapFromPinState(pin));
+            }
+        }
         return l;
     }
 
@@ -190,7 +155,9 @@ public class RestGpioController {
      */
     @PreDestroy
     public void shutdown() {
-        gpio.shutdown();
+        synchronized (gpio) {
+            gpio.shutdown();
+        }
     }
 
     private Map<String, Object> mapFromPinState(final GpioPin pin) {
